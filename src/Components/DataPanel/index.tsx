5ea78a9492e1aa8a780fb08a5cfb5f7f31ba2060
David Morgan <haitheredavid@gmail.com>
2025-02-02 06:54:05 -0500

    Resize

--- src/Components/DataPanel/index.tsx
+++ src/Components/DataPanel/index.tsx
@@ -1,20 +1,26 @@
+import { useRef } from 'react';
 import {observer} from 'mobx-react';
 import { Panel } from 'primereact/panel';
 
 import { useStores } from '../../stores';
+import useResize from '../../hooks/useResize';
 import SpaceTreemap from "../TreeMap/SpaceTreemap";
 import { ManualScatter } from "../ManualScatter/ManualScatter";
 
 export default observer(function DataPanel() {
     const { entities } = useStores();
+    const container = useRef<HTMLDivElement>(null);
+    const size = useResize(container);
 
     return (
         <Panel className="DataPanel" header="Model Data">
-            {/*}
-            <ManualScatter width={800} height={400}/>
-            <SpaceTreemap width={800} height={400}
-                          treeTotals={entities.activeTreeMap} />
-              */}
+            <div className="DataPanel__container" ref={container}>
+                <ManualScatter width={size.height * 2} height={size.height}/>
+                {/*}
+                <SpaceTreemap width={800} height={400}
+                              treeTotals={entities.activeTreeMap} />
+                  */}
+            </div>
         </Panel>
     );
 });
